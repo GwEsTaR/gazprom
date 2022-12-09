fbcd3187f7c4be4a1440c8fab1c0a60afd05fe52
VanimaDa <97492115+VanimaDa@users.noreply.github.com>
2022-12-09 10:49:50 +0700

    Corups
    Start

---
+++ corpus.py
@@ -0,0 +1,21 @@
+from tkinter import *
+from tkinter import ttk
+
+
+root = Tk()
+
+root["bg"]='white'
+root.title('Регистратура')
+root.wm_attributes('-alpha', 1)
+root.geometry('400x100')
+
+root.resizable(width=False, height=False)
+
+entry = ttk.Entry()
+entry.pack(anchor=NW, padx=6, pady=6)
+
+
+btn = Button(root, text='da')
+btn.pack()
+
+root.mainloop()
